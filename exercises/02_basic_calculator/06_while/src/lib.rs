ab570ed9647346f5471ebe91ed5ffe9d1976d7b5
Abeer Sud <abeersud@macbookpro.home>
2025-07-07 18:37:04 -0400

    all files

---
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -0,0 +1,39 @@
+// Rewrite the factorial function using a `while` loop.
+pub fn factorial(n: u32) -> u32 {
+    // The `todo!()` macro is a placeholder that the compiler
+    // interprets as "I'll get back to this later", thus
+    // suppressing type errors.
+    // It panics at runtime.
+    let mut n = n;
+    let mut fact: u32 = 1;
+    while n > 0 {
+        fact *= n;
+        n -= 1;
+    }
+    return fact;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::factorial;
+
+    #[test]
+    fn first() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn second() {
+        assert_eq!(factorial(1), 1);
+    }
+
+    #[test]
+    fn third() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn fifth() {
+        assert_eq!(factorial(5), 120);
+    }
+}
